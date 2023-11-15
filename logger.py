b977234a6e48be76c60e5bab6afdc2c6bf9738fd
wjddla <97452167+Nyounggu@users.noreply.github.com>
2023-11-16 00:09:18 +0900

    logger.py

--- logger.py
+++ logger.py
@@ -5,10 +5,11 @@ class Logger(object):
     """Tensorboard logger."""
 
     def __init__(self, log_dir):
-        """Initialize summary writer."""
-        self.writer = tf.summary.FileWriter(log_dir)
+      """Initialize summary writer."""
+      self.writer = tf.summary.create_file_writer(log_dir)
 
     def scalar_summary(self, tag, value, step):
-        """Add scalar summary."""
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+      """Add scalar summary."""
+      with self.writer.as_default():
+        tf.summary.scalar(tag, value, step)
+        self.writer.flush()
